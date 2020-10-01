c42e01dae0298c8c32c793ed8bda70c06d4d9b3e
jose angel linarez <ydauracontreras@gmail.com>
2020-09-30 22:44:00 -0300

    Updates

---
+++ precache-manifest.2b4e5f8e0ae897d042357f1d661fb04d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2751e738d63e279f1f342b680122b0",
+    "url": "/piano-react-app/index.html"
+  },
+  {
+    "revision": "8268670865a2590512be",
+    "url": "/piano-react-app/static/css/main.1f9eef54.chunk.css"
+  },
+  {
+    "revision": "71b0edf539b10c5c8de8",
+    "url": "/piano-react-app/static/js/2.1ae3735d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/piano-react-app/static/js/2.1ae3735d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8268670865a2590512be",
+    "url": "/piano-react-app/static/js/main.ba489681.chunk.js"
+  },
+  {
+    "revision": "2d902ac869a42a24542c",
+    "url": "/piano-react-app/static/js/runtime-main.cb6e1bc5.js"
+  },
+  {
+    "revision": "095eab302ab8006c2a2d23bad3621fc4",
+    "url": "/piano-react-app/static/media/descarga-arrow.095eab30.svg"
+  }
+]);
