83926594473974a40358afe136f67fc0529da097
jose angel linarez <ydauracontreras@gmail.com>
2020-09-30 22:26:18 -0300

    se agrego container principal usando los componentes de material ui

--- piano-app/src/Piano.js
+++ piano-app/src/Piano.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import Instrument from "./Instrument";
 import flecha from "./assets/img/descarga-arrow.svg";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
 const Piano = () => {
@@ -61,70 +60,71 @@ const Piano = () => {
   };
 
   return (
-    <div className="pricipal-container">
-      <Container maxWidth="sm">
-        <div className="title">
-          <h1>React-Piano</h1>
-          <p>Teclado de piano interactivo hecho con react 🎹</p>
-          <div className="title-link">
-            <a href="https://github.com/Ydauracontreras/piano-react-app">
-              {" "}
-              GITHUB
-            </a>
-          </div>
+    <Container>
+      <CssBaseline />
+
+      <div className="title">
+        <h1>React-Piano</h1>
+        <p>Teclado de piano interactivo hecho con react 🎹</p>
+        <div className="title-link">
+          <a href="https://github.com/Ydauracontreras/piano-react-app">
+            {" "}
+            GITHUB
+          </a>
         </div>
-        <div className="mesagge">
-          <p>Pruebalo haciendo clic, tocando o usando su teclado:</p>
-          <img className="flecha" src={flecha} />
-        </div>
-        <div className="piano-container">
-          <Instrument
-            instrumentName={"acoustic_grand_piano"}
-            startNote={"C3"}
-            endNote={"B5"}
-            renderPianoKey={renderPianoKey}
-            keyboardMap={{
-              Q: "C3",
-              2: "C#3",
-              W: "D3",
-              3: "D#3",
-              E: "E3",
-              R: "F3",
-              5: "F#3",
-              T: "G3",
-              6: "G#3",
-              Y: "A3",
-              7: "A#3",
-              U: "B3",
-              I: "C4",
-              9: "C#4",
-              O: "D4",
-              0: "D#4",
-              P: "E4",
-              Z: "F4",
-              S: "F#4",
-              X: "G4",
-              D: "G#4",
-              C: "A4",
-              F: "A#4",
-              V: "B4",
-              B: "C5",
-              H: "C#5",
-              N: "D5",
-              J: "D#5",
-              M: "E5",
-              ",": "F5",
-              L: "F#5",
-              ".": "G5",
-              ";": "G#5",
-              "/": "A5",
-              "'": "A#5",
-              A: "B5",
-            }}
-          />
-        </div>
-      </Container>
-    </div>
+      </div>
+      <div className="mesagge">
+        <p>Pruebalo haciendo clic, tocando o usando su teclado:</p>
+        <img className="flecha" src={flecha} />
+      </div>
+
+      <div className="piano-container">
+        <Instrument
+          instrumentName={"acoustic_grand_piano"}
+          startNote={"C3"}
+          endNote={"B5"}
+          renderPianoKey={renderPianoKey}
+          keyboardMap={{
+            Q: "C3",
+            2: "C#3",
+            W: "D3",
+            3: "D#3",
+            E: "E3",
+            R: "F3",
+            5: "F#3",
+            T: "G3",
+            6: "G#3",
+            Y: "A3",
+            7: "A#3",
+            U: "B3",
+            I: "C4",
+            9: "C#4",
+            O: "D4",
+            0: "D#4",
+            P: "E4",
+            Z: "F4",
+            S: "F#4",
+            X: "G4",
+            D: "G#4",
+            C: "A4",
+            F: "A#4",
+            V: "B4",
+            B: "C5",
+            H: "C#5",
+            N: "D5",
+            J: "D#5",
+            M: "E5",
+            ",": "F5",
+            L: "F#5",
+            ".": "G5",
+            ";": "G#5",
+            "/": "A5",
+            "'": "A#5",
+            A: "B5",
+          }}
+        />
+      </div>
+    </Container>
   );
 };
 
